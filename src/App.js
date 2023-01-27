28ae1fb1dc94ffb7bb17ebb321fc7943638b521e
wjdals95 <wjdals992@naver.com>
2023-01-27 11:36:57 +0900

    loading & Data State처리

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   //Fetch API를 사용하여 영화 Data가져오기
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map(movieData => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releasDate: movieData.release_date
-          }
-        })
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    //비동기화 코드로 변환
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    // .then((response) => {
+    //   return response.json();
+    // })
+    // .then((data) => {
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releasDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   // const dummyMovies = [
@@ -46,7 +49,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {/* 로딩중이라는 것을 하기위해 useState를 사용해서 isLoading이 false일때는
+        MoviesList가 나오고 true일때는 로딩중이라고 나온다. */}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length ===0 && <p>Found no movies...</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
